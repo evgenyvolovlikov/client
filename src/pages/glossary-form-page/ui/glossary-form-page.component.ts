68df3dfd5921ae640bdae224717780bdce433714
Евгений Воловликов <evgenyvolovlikov@gmail.com>
2026-06-18 22:26:45 +0300

    feat: implement responsive admin article form based on data model

--- src/pages/glossary-form-page/ui/glossary-form-page.component.ts
+++ src/pages/glossary-form-page/ui/glossary-form-page.component.ts
@@ -1,4 +1,5 @@
-import {ChangeDetectionStrategy, Component, computed, input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, computed, input} from '@angular/core';
+import {FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {RouterLink} from '@angular/router';
 
 @Component({
@@ -7,21 +8,79 @@ import {RouterLink} from '@angular/router';
     changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: 'glossary-form-page.component.html',
     styleUrl: 'glossary-form-page.component.scss',
-    imports: [RouterLink],
+    imports: [RouterLink, ReactiveFormsModule],
 })
-export class GlossaryFormPageComponent {
+export class GlossaryFormPageComponent implements OnInit {
     public id = input<string>();
     protected isEditMode = computed(() => !!this.id());
-    protected pageTitle = computed(() => {
-        return this.isEditMode() ? `Редактирование статьи: ${this.id()}` : 'Создание новой статьи';
+
+    protected form = new FormGroup({
+        category: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+        title: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+        leadText: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+        noteText: new FormControl('', {nonNullable: true}),
+        sectionTitle: new FormControl('', {nonNullable: true, validators: Validators.required}),
+
+        features: new FormArray<FormGroup<{title: FormControl<string>; text: FormControl<string>}>>(
+            [],
+        ),
+
+        toc: new FormArray<
+            FormGroup<{
+                id: FormControl<string>;
+                label: FormControl<string>;
+                anchor: FormControl<string>;
+            }>
+        >([]),
     });
 
-    protected onSubmit(event: Event): void {
-        event.preventDefault();
+    protected get featuresFormArray() {
+        return this.form.controls.features;
+    }
+
+    protected get tocFormArray() {
+        return this.form.controls.toc;
+    }
+
+    public ngOnInit(): void {
+        this.addFeature();
+        this.addTocElement();
+
         if (this.isEditMode()) {
-            console.warn(`Сохраняем изменения для старой статьи: ${this.id()}`);
-        } else {
-            console.warn('Создаем абсолютно новую статью');
+            this.form.patchValue({
+                category: 'Психология эмоций',
+                title: 'Понятие эмоций: функции, виды и природа',
+                leadText: 'Эмоции — это кратковременные субъективные реакции человека...',
+                sectionTitle: 'Основные функции эмоций',
+            });
         }
+    }
+
+    protected addFeature(): void {
+        this.featuresFormArray.push(
+            new FormGroup({
+                title: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+                text: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+            }),
+        );
+    }
+
+    protected addTocElement(): void {
+        this.tocFormArray.push(
+            new FormGroup({
+                id: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+                label: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+                anchor: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
+            }),
+        );
+    }
+
+    protected onSubmit(): void {
+        if (this.form.invalid) {
+            this.form.markAllAsTouched();
+            return;
+        }
+
+        console.log('Сформированный Payload по модели Article:', this.form.getRawValue());
     }
 }
