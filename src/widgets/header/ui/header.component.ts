e32a06133d55cce9f78d20674c36ca2eafcb0d66
Евгений Воловликов <evgenyvolovlikov@gmail.com>
2026-06-19 20:21:19 +0300

    refactor: update header comp, extract layout heights, font weights and modular font sizes to tokens

--- src/widgets/header/ui/header.component.ts
+++ src/widgets/header/ui/header.component.ts
@@ -1,12 +1,27 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
     selector: 'app-w-header',
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [RouterLink],
+    imports: [RouterLink, RouterLinkActive],
     templateUrl: 'header.component.html',
     styleUrl: 'header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    protected readonly isLightTheme = signal<boolean>(false);
+
+    constructor() {
+        const body = document.body;
+        if (this.isLightTheme()) {
+            body.classList.add('light-theme');
+        } else {
+            body.classList.remove('light-theme');
+        }
+    }
+
+    protected toggleTheme(): void {
+        this.isLightTheme.update((state) => !state);
+    }
+}
